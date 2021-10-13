67fb705269607613b055d5bb12f55f81548375ff
PhiDCH <59147911+PhiDCH@users.noreply.github.com>
2021-10-13 15:41:14 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
+mkdir checkpoint pretrained
+gdown --id 1dXMKs1VAltre6RCtDHYft-2q8oCtC_Yh -O checkpoint/pan.pth.tar
 wget -O pretrained/resnet18-imagenet.pth http://sceneparsing.csail.mit.edu/model/pretrained_resnet/resnet18-imagenet.pth
 cd ./models/post_processing/pa/
 python setup.py build_ext --inplace
