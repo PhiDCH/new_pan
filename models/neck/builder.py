8afe43fba679f636641ffcb52dc3a6c9236c9f5a
PhiDCH <59147911+PhiDCH@users.noreply.github.com>
2021-10-13 15:58:56 +0700

    Update builder.py

--- models/neck/builder.py
+++ models/neck/builder.py
@@ -1,4 +1,5 @@
-import models
+# import models
+from .fpem_v1 import FPEM_v1
 
 
 def build_neck(cfg):
@@ -8,6 +9,7 @@ def build_neck(cfg):
             continue
         param[key] = cfg[key]
 
-    neck = models.neck.__dict__[cfg.type](**param)
+    # neck = models.neck.__dict__[cfg.type](**param)
+    neck = FPEM_v1(**param)
 
     return neck
